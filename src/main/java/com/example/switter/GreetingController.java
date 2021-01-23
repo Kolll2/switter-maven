4466b68b891342c91f3d38a23cefdc742e2438f2
m.doronin <m.doronin@haulmont.com>
2021-01-23 15:45:52 +0400

    add mustache support

--- src/main/java/com/example/switter/GreetingController.java
+++ src/main/java/com/example/switter/GreetingController.java
@@ -5,17 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
+        model.put("name", name);
         return "greeting";
     }
 
     @GetMapping
-    public String main(@RequestParam String text, @RequestParam String tag, Model model) {
+    public String main(@RequestParam(name = "text", required = false, defaultValue = "Default World") String text ,
+                       @RequestParam(name = "tag", required = false) String tag,
+                       Map<String, Object> model) {
+        model.put("name", text);
         return "main";
     }
 }
